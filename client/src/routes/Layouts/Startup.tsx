9bc0bf5be0f2257ef339767a4caf4f8b56b260e4
Michelle Ward <--global>
2025-03-26 21:38:00 -0400

    auth landing logo and welcome message

--- client/src/routes/Layouts/Startup.tsx
+++ client/src/routes/Layouts/Startup.tsx
@@ -7,7 +7,7 @@ import AuthLayout from '~/components/Auth/AuthLayout';
 import { REDIRECT_PARAM, SESSION_KEY } from '~/utils';
 
 const headerMap: Record<string, TranslationKeys> = {
-  '/login': 'com_auth_welcome_back',
+  '/login': 'Welcome to Edge' as TranslationKeys,
   '/register': 'com_auth_create_account',
   '/forgot-password': 'com_auth_reset_password',
   '/reset-password': 'com_auth_reset_password',
